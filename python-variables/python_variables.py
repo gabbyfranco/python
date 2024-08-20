40499faf25265e2190fec93056c582311bab17e7
Lea Franco <leafranco@Leas-iMac.local>
2024-08-20 15:14:50 -0600

    creating variables in python

---
+++ python-variables/python_variables.py
@@ -0,0 +1,27 @@
+#DONE: naming variables.
+x = 'hello'
+
+#DONE: print variables.
+print(x)
+
+#DONE: print three values separately.
+
+# examples
+a,b,c = 2,3,4
+
+#DONE: print variables
+print(a)
+print(b)
+print(c)
+
+#TODO: print out 2 different values from same variables
+
+#examples
+
+a = 10
+
+print(a)
+
+del a 
+
+print(a)
