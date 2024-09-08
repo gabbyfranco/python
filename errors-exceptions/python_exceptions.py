8e188dbd1dff7fa364666e09fbcad0832e704ed4
Lea Franco <leafranco@Leas-iMac.local>
2024-09-08 16:02:00 -0600

    errors and exceptions

---
+++ errors-exceptions/python_exceptions.py
@@ -0,0 +1,13 @@
+# Exceptions
+# Write a simple math code
+
+# DONE: define a new function called divide by and allow it to accept two parameters, A & B.
+
+def divide_by(a_b):# The purpose of the function is to return the value of the division of both numbers.
+    return a / b 
+
+#DONE: Add a print statement for the divide by function
+try:
+    ans = divide_by(40, 0)
+except:
+    print("AUR NAUR!!! SOMETHING WENT WRONGGGGG")
---
+++ functions-data/sets.py
@@ -0,0 +1,5 @@
+set_a = {1, 2, 3, 4, 5}
+# set_a.remove(2)
+set_b = {4, 5, 6, 7, 8}
+
+print(set_a - set_b)
